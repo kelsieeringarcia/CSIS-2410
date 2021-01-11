21201c049cdefcd639d462a7f4b868e299455dd2
kelsieeringarcia <36800046+kelsieeringarcia@users.noreply.github.com>
2021-01-11 11:34:48 -0700

    Add files via upload

---
+++ LinkedStack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinkedStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("A");
+            stack.Push("B");
+            stack.Push("C");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Stack<int> stack2 = new Stack<int>();
+            Console.WriteLine(stack.Pop());
+            stack2.Push(10);
+            stack2.Push(12);
+            stack2.Push(20);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
---
+++ LinkedStack/Stack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace LinkedStack
+{
+    public class Stack<T>
+    {
+        private Node top;
+        private int size;
+        public Stack()
+        {
+            top = null;
+            size = 0;
+        }
+
+        private class Node
+        {
+            internal T item;
+            internal Node next;
+
+        }
+
+        public void Push(T item)
+        {
+            Node newNode = new Node();
+            newNode.item = item;
+            if (isEmpty())
+                newNode.next = null;
+            else
+                newNode.next = top;
+
+            top = newNode;
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException("Can't pop from an empty stack.");
+            T topItem = top.item;
+            top = top.next;
+            size--;
+            return topItem;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            Node current = top;
+            while(current != null)
+            {
+                sb.Append(current.item).Append(" ");
+                current = current.next;
+            }
+
+            return sb.ToString();
+        }
+
+    }
+}
